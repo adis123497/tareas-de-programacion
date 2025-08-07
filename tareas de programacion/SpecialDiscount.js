9b7c3a1d620020369c12913c2640613f2145adaa
ladis2007 <albertoladis376@gmail.com>
2025-08-07 13:41:18 -0600

    Update SpecialDiscount.js

---
+++ tareas de programacion/SpecialDiscount.js
@@ -0,0 +1,8 @@
+let edad = 65; // edad del cliente
+let discapacitado = false; // true si tiene discapacidad
+
+if (edad > 60 || discapacitado) {
+  console.log("Recibe descuento especial.");
+} else {
+  console.log("No recibe descuento especial.");
+}
