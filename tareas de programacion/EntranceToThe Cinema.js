fd94c16af34bc547f3ff1c750419ec678edc3b10
ladis2007 <albertoladis376@gmail.com>
2025-08-07 13:42:00 -0600

    Update EntranceToThe Cinema.js

---
+++ tareas de programacion/EntranceToThe Cinema.js
@@ -0,0 +1,8 @@
+let entradaValida = true; //true si tiene entrada
+let edad = 14; // edad de la persona
+
+if (entradaValida && edad > 13) {
+  console.log("Puede entrar al cine.");
+} else {
+  console.log("No puede entrar al cine.");
+}
