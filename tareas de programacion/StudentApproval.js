31b6ad2e84a1817278e5c112d167e837562f3cd8
ladis2007 <albertoladis376@gmail.com>
2025-08-07 13:40:30 -0600

    Update StudentApproval.js

---
+++ tareas de programacion/StudentApproval.js
@@ -0,0 +1,8 @@
+let nota = 6.5; // nota final
+let pasoRecuperacion = true; // true si aprobó recuperación
+
+if (nota >= 7 || pasoRecuperacion) {
+  console.log("El estudiante aprueba.");
+} else {
+  console.log("El estudiante no aprueba.");
+}
